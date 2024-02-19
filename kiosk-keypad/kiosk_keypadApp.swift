7e2ed30f2c2615cab2c290bf3a989e927e77ac62
Sarah <144402200+chimmies@users.noreply.github.com>
2024-02-18 19:13:19 -0800

    Initial Commit

---
+++ kiosk-keypad/kiosk_keypadApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiosk_keypadApp.swift
+//  kiosk-keypad
+//
+//  Created by Sarah Munguia on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct kiosk_keypadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
